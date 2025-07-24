7f26a12c5aa0132e3fa1869de3dcfb077ff42be5
Todays Dental Partners <todaysdentalpartners@gmail.com>
2025-07-24 20:13:02 +0530

    Initial commit

---
+++ test/denti_pal_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DentiPalCdk from '../lib/denti_pal_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/denti_pal_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DentiPalCdk.DentiPalCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
